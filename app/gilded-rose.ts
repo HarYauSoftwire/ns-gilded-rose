268d99dbdbcf935d90358d896287cfe71cd5b4c4
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 17:27:52 +0100

    add new increaseQuality method

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,35 +17,29 @@ export class GildedRose {
         this.items = items;
     }
 
+    private increaseQuality(originalQuality: number, change: number) {
+        return Math.max(originalQuality, Math.min(originalQuality + change, 50));
+    }
+
     updateQuality() {
         for (const item of this.items) {
             if (item.name === 'Sulfuras, Hand of Ragnaros') {
                 continue;
             }
             if (item.name == 'Aged Brie') {
-                if (item.quality < 50) {
-                    item.quality += 1;
-                }
-                if (item.sellIn <= 0) {
-                    if (item.quality < 50) {
-                        item.quality += 1;
-                    }
+                if (item.sellIn > 0) {
+                    item.quality = this.increaseQuality(item.quality, 1);
+                } else {
+                    item.quality = this.increaseQuality(item.quality, 2);
                 }
             } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality < 50) {
-                    item.quality += 1;
-                }
-                if (item.sellIn < 11) {
-                    if (item.quality < 50) {
-                        item.quality += 1;
-                    }
-                }
-                if (item.sellIn < 6) {
-                    if (item.quality < 50) {
-                        item.quality += 1;
-                    }
-                }
-                if (item.sellIn <= 0) {
+                if (item.sellIn > 10) {
+                    item.quality = this.increaseQuality(item.quality, 1);
+                } else if (item.sellIn > 5) {
+                    item.quality = this.increaseQuality(item.quality, 2);
+                } else if (item.sellIn > 0) {
+                    item.quality = this.increaseQuality(item.quality, 3);
+                } else {
                     item.quality = 0;
                 }
             } else {
