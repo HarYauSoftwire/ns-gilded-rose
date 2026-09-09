c32aeb463a13a5e91bfc9fd5adcc89de2de71fca
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 16:54:42 +0100

    use for-of loop

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,8 +18,7 @@ export class GildedRose {
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            const item = this.items[i];
+        for (const item of this.items) {
             if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.quality > 0) {
                     if (item.name != 'Sulfuras, Hand of Ragnaros') {
