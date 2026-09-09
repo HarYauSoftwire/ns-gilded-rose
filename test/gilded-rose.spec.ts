6903c09c04a192103007d66d74c3e29717d42485
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 16:45:22 +0100

    add unit tests for normal items

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -15,4 +15,40 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('Foo');
     });
 
+    it ('should lower the sell-in of a normal item by 1', function () {
+        const gildedRose = new GildedRose([new Item('Foo', 12, 13)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.eql(11);
+    });
+
+    it ('should lower the quality of a normal item with positive quality by 1', function () {
+        const gildedRose = new GildedRose([new Item('Foo', 12, 13)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.eql(12);
+    });
+
+    it ('should not lower the quality of a normal item with zero quality', function () {
+        const gildedRose = new GildedRose([new Item('Foo', 12, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.eql(0);
+    });
+
+    it ('should lower the quality of an out-of-date normal item with positive quality by 2', function () {
+        const gildedRose = new GildedRose([new Item('Foo', -3, 13)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.eql(11);
+    });
+
+    it ('should not lower the quality of an out-of-date normal item with zero quality', function () {
+        const gildedRose = new GildedRose([new Item('Foo', -3, 0)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.eql(0);
+    });
+
+    it ('should not lower the quality of an out-of-date normal item with 1 quality past zero', function () {
+        const gildedRose = new GildedRose([new Item('Foo', -3, 1)]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.eql(0);
+    });
+
 });
