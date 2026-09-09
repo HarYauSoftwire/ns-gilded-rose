404a28bd619b951da8d6a425acef9abd361b668f
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 17:11:42 +0100

    invert some if statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,11 +22,7 @@ export class GildedRose {
             if (item.name === 'Sulfuras, Hand of Ragnaros') {
                 continue;
             }
-            if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (item.quality > 0) {
-                    item.quality -= 1;
-                }
-            } else {
+            if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
                 if (item.quality < 50) {
                     item.quality += 1;
                     if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -42,20 +38,24 @@ export class GildedRose {
                         }
                     }
                 }
+            } else {
+                if (item.quality > 0) {
+                    item.quality -= 1;
+                }
             }
             item.sellIn -= 1;
             if (item.sellIn < 0) {
-                if (item.name != 'Aged Brie') {
-                    if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.name == 'Aged Brie') {
+                    if (item.quality < 50) {
+                        item.quality += 1;
+                    }
+                } else {
+                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        item.quality = 0;
+                    } else {
                         if (item.quality > 0) {
                             item.quality -= 1;
                         }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality += 1;
                     }
                 }
             }
