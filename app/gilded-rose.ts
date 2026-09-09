1926a9764d3d7e41b1e2c98e791664b84eb21e5c
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 17:13:59 +0100

    use else-if statements

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,20 +22,22 @@ export class GildedRose {
             if (item.name === 'Sulfuras, Hand of Ragnaros') {
                 continue;
             }
-            if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.name == 'Aged Brie') {
                 if (item.quality < 50) {
                     item.quality += 1;
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality += 1;
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality += 1;
-                            }
-                        }
+                }
+            } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.quality < 50) {
+                    item.quality += 1;
+                }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality += 1;
+                    }
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality += 1;
                     }
                 }
             } else {
@@ -49,13 +51,11 @@ export class GildedRose {
                     if (item.quality < 50) {
                         item.quality += 1;
                     }
+                } else if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                    item.quality = 0;
                 } else {
-                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        item.quality = 0;
-                    } else {
-                        if (item.quality > 0) {
-                            item.quality -= 1;
-                        }
+                    if (item.quality > 0) {
+                        item.quality -= 1;
                     }
                 }
             }
