793488e0a8af7e3e38f62f2e3bba0f787fb95c75
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 17:16:18 +0100

    change sellIn at the end

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -45,8 +45,7 @@ export class GildedRose {
                     item.quality -= 1;
                 }
             }
-            item.sellIn -= 1;
-            if (item.sellIn < 0) {
+            if (item.sellIn <= 0) {
                 if (item.name == 'Aged Brie') {
                     if (item.quality < 50) {
                         item.quality += 1;
@@ -59,6 +58,7 @@ export class GildedRose {
                     }
                 }
             }
+            item.sellIn -= 1;
         }
 
         return this.items;
