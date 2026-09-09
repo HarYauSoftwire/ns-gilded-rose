18eadde33e1f48f165138d60b8a147b4cfd8dbb0
Harvey Yau <Harvey.Yau@softwire.com>
2026-09-09 16:10:12 +0100

    add a spec test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -6,7 +6,13 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal('foo');
+    });
+
+    it('should store the item we supplied it', function() {
+        const gildedRose = new GildedRose([ new Item('Foo', 0, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal('Foo');
     });
 
 });
